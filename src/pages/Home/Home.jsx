f2847e81250e7e96b8674ca052382750978ceb4d
Damian Padilla <damipad4@gmail.com>
2023-08-25 01:59:28 -0500

    init styled components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
+import { Container } from "../../components/Container.styled"
+
 export default function Home() {
     return(
-        <>
-        </>
+        <Container>
+            <h1>Home Page</h1>
+        </Container>
     )
 }
