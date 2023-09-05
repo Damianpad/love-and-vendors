510f25c94746fcbf81698fd95ba50fb173c868f2
Damian Padilla <damipad4@gmail.com>
2023-09-05 01:12:28 -0500

    basic content laid out for home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -2,8 +2,36 @@ import { Container } from "../../styles/Container.styled";
 
 export default function Home() {
   return (
-    <Container>
+    <>
+      <Container>
+        <span>Navigation</span>
+        <button>Log in</button>
+      </Container>
+
+      <img src="src\assets\img\dancing_centerU.png" alt="" />
       <h1>Home Page</h1>
-    </Container>
+
+      <section>
+        <h2>Finding what you need all in one place</h2>
+        <button>Find More</button>
+      </section>
+
+      <section>
+        <span>Videographer Vendor</span>
+        <span>Photographer Vendor</span>
+        <span>Venues Vendor</span>
+      </section>
+
+      <section>
+        <img src="src\assets\img\LV_1.jpg" alt="" />
+        <h2>Discover the best in your area</h2>
+        <h3>Find the perfect vendor for your day.</h3>
+        <button>Get Started</button>
+      </section>
+
+      <section>
+        <span>Footer Component</span>
+      </section>
+    </>
   );
 }
