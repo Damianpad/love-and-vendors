0471f7b0b8bddd94471083c6432e55e728bffe96
Damian Padilla <damipad4@gmail.com>
2023-09-08 12:19:37 -0500

    updated home style

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,14 +1,22 @@
 import { Container } from "../../styles/Container.styled";
+import { HomeStyled } from "./Home.styled";
 
 export default function Home() {
   return (
-    <>
+    <HomeStyled>
       <Container>
         <span>Navigation</span>
         <button>Log in</button>
       </Container>
 
-      <img src="src\assets\img\dancing_centerU.png" alt="" />
+      <section className="headerContainer">
+        {/* <img
+          src="src\assets\img\dancing_centerU.png"
+          alt=""
+          className="headerImg"
+        /> */}
+      </section>
+
       <h1>Home Page</h1>
 
       <section>
@@ -23,7 +31,7 @@ export default function Home() {
       </section>
 
       <section>
-        <img src="src\assets\img\LV_1.jpg" alt="" />
+        <img src="src\assets\img\LV_1.jpg" alt="" className="ctaImg" />
         <h2>Discover the best in your area</h2>
         <h3>Find the perfect vendor for your day.</h3>
         <button>Get Started</button>
@@ -32,6 +40,6 @@ export default function Home() {
       <section>
         <span>Footer Component</span>
       </section>
-    </>
+    </HomeStyled>
   );
 }
---
+++ src/components/pages/Home/Home.styled.js
@@ -0,0 +1,25 @@
+import { styled } from "styled-components";
+import dancingCouple from "../../../assets/img/dancing_centerU.png";
+
+export const HomeStyled = styled.div`
+  width: 100%;
+  color: black;
+
+  .headerContainer {
+    width: 100%;
+    height: 100vh;
+    background-image: url(${dancingCouple});
+    background-repeat: no-repeat;
+    background-position: center; 
+  }
+  .headerImg {
+    width: 100%;
+    object-fit: cover;
+    height: 100vh;
+  }
+
+  .ctaImg {
+    width: 100%;
+    border-radius: 15px;
+  }
+`;
