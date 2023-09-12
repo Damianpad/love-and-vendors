367bf26559523dc1ea97f03b8bc98e64131e897d
Damian Padilla <damipad4@gmail.com>
2023-09-12 10:00:03 -0500

    init getting started styles

--- src/components/pages/GettingStarted/GettingStarted.jsx
+++ src/components/pages/GettingStarted/GettingStarted.jsx
@@ -1,21 +1,23 @@
-import StartedImage from "../../StartedImage/StartedImage"
+import StartedImage from "../../StartedImage/StartedImage";
+import { GettingStartedStyles } from "./GettingStarted.styled";
 
-export default function GettingStarted(){
-    return(
-        <>
-            <section >
-                <section>
-                    <StartedImage />
-                </section>
-                <section>
-                    <span>step component</span>
-                    <h2>Welcome to Love & Vendors! Let's get you to the right place.</h2>
-                    <span>Getting Started Component</span>
-                    <span>Already have an account?
-                    <a href="">Log in</a>    
-                    </span>
-                </section>
-            </section>
-        </>
-    )
+export default function GettingStarted() {
+  return (
+    <GettingStartedStyles>
+      <section>
+        <section>
+          <StartedImage />
+        </section>
+        <section>
+          <span>step component</span>
+          <h2>Welcome to Love & Vendors! Let's get you to the right place.</h2>
+          <span>Getting Started Component</span>
+          <span>
+            Already have an account?
+            <a href="">Log in</a>
+          </span>
+        </section>
+      </section>
+    </GettingStartedStyles>
+  );
 }
