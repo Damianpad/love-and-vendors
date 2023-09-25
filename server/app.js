e0815bd65b902a13373c394145f20496e5a64a4c
Damian Padilla <damipad4@gmail.com>
2023-09-24 22:32:54 -0500

    Update: server now serves frontend when running

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ const app = express()
 
 const port = process.env.PORT || 5000;
 
-app.get('/')
 
-app.use(express.static('../client/dist'));
+// Serve the React frontend
+app.use(express.static('../client/dist')); // Adjust the path as needed
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
