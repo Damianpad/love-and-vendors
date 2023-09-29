f7dd5a1b93a34a5f191ba057a9fd3c3e9677946b
Damian Padilla <damipad4@gmail.com>
2023-09-28 23:00:39 -0500

    update file structure

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const port = process.env.PORT || 5000;
 
-
 // Serve the React frontend
-app.use(express.static('../client/dist')); // Adjust the path as needed
+app.use(express.static("../client/dist")); // Adjust the path as needed
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`
+ |   |                                         |    |
+ |   | Server is up and running on port ${port}🚀 |    |
+ |   |_________________________________________|    |
+ |                                                  |
+  \_________________________________________________/
+         \___________________________________/
+      ___________________________________________
+   _-\'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- \`-_
+_-\'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.\`-_
+_-\'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-\`__\`. .-.-.-.\`-_
+  `);
+});
