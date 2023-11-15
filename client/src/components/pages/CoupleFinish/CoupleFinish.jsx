63da34ce7f5d8b3cd69e9b7e84dc3d06be188dd3
Damian Padilla <damipad4@gmail.com>
2023-11-15 00:01:24 -0600

    fix: fix alignment for couple finish page

--- client/src/components/pages/CoupleFinish/CoupleFinish.jsx
+++ client/src/components/pages/CoupleFinish/CoupleFinish.jsx
@@ -5,9 +5,16 @@ import GettingStartedImage from "../../GettingStartedImage/GettingStartedImage";
 export default function CoupleFinish() {
   return (
     <CoupleFinishStyles>
+      <section className="FinishContainer">
       < GettingStartedImage />
+
+      <section className="stepContainer">
       <GSStep />
       <h1>Couple Info</h1>
+      </section>
+ 
+      </section>
+
     </CoupleFinishStyles>
   );
 }
--- client/src/components/pages/CoupleFinish/CoupleFinish.styled.js
+++ client/src/components/pages/CoupleFinish/CoupleFinish.styled.js
@@ -2,4 +2,22 @@ import styled from "styled-components";
 
 export const CoupleFinishStyles = styled.div`
     color: black;
+
+
+    .FinishContainer {
+display: flex;
+    }
+
+    .stepContainer {
+        margin: 0 auto;
+        width: 50%;
+    }
+
+    @media (min-width: 1000px) {
+    .FinishContainer{
+
+      
+
+    }
+  }
 `;
